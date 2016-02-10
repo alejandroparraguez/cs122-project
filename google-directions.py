2e8dc03d8491fc74b409995f3316e9baf750b5e2
AlyssaBlack <ablackburn5@gmail.com>
2016-02-09 19:38:58 -0600

    taxi fare

--- google-directions.py
+++ google-directions.py
@@ -33,3 +33,33 @@ print('start long', start_long)
 print('end lat', end_lat)
 print('end long', end_long)
 
+def calc_cab_fare(file, sec, mile, num_pass):
+	with open('IL_taxi.json') as data_file:
+		data = json.load(data_file)
+	base_fare = data['base_fare']
+	per_mile = data["per_mile"]
+	per_min = data["per_min"]
+	first_passenger = data["first_passenger"]
+	add_passenger = data["additional_passenger"]
+	
+	clean_fee = data["clean_fee"]
+	airport_tax = data["airport_tax"]
+	
+	if num_pass > 1:
+		pass_fare = first_passenger + add_passenger * (num_pass - 1)
+	else:
+		pass_fare = 0
+	
+	return base + (per_mile * mile) + (per_min * mile) + pass_fare
+
+sec = 0
+miles = 0
+for route in data['routes']:
+	leg = route['leg']
+	duration = leg['duration']
+	distance = leg['distance']
+	
+	sec += duration['value']
+	miles += duration['value']
+	
+	
